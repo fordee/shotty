63e6bb81bb91b72bea200b126834c3146cfd24ef
John Forde <fordee@me.com>
2018-03-03 15:23:19 +1300

    Bug fix

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -17,7 +17,7 @@ def filter_instances(project):
 
 def has_pending_snapshot(volume):
     snapshots = list(volume.snapshots.all())
-    return snapshots and snapshots[0].state == 'pending':
+    return snapshots and snapshots[0].state == 'pending'
 
 
 @click.group()
@@ -102,7 +102,7 @@ def create_snapshots(project):
         i.wait_until_stopped()
         for v in i.volumes.all():
             if has_pending_snapshot(v):
-                print("  Skipping. Snapshot {0} already in progress.".format(v.id))
+                print("  Skipping. Snapshot for {0} already in progress.".format(v.id))
                 continue
             print("  Creating snapshot for {0}...".format(v.id))
             v.create_snapshot(Description="Created by SnapshotAlyzer 3000")
